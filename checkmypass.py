c2ee377e6a3c34227791f6bbabd5a7144bbb4cf9
Vikasa M R <65649086+Vikasmax@users.noreply.github.com>
2021-11-27 13:37:00 +0530

    Update checkmypass.py

--- checkmypass.py
+++ checkmypass.py
@@ -6,7 +6,7 @@ def api_request_data(query_char):
 	url = 'https://api.pwnedpasswords.com/range/' + query_char
 	res = requests.get(url)
 	if res.status_code != 200:
-		raise RuntimeError(f"error fetching: {res.status_code}, check the api ")
+		raise RuntimeError(f"error fetching the site, check the api ")
 	return res  
 
 def password_leaks_count(hashes, hash_to_check):
